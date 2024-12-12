663d88ba98d48445c030873398e6f631aeb826c3
Vamilla4 <evanaradhy@gmail.com>
2024-12-12 14:50:46 +0700

    Commit Migration and models
    Migration + models

---
+++ database/migrations/2024_12_12_072622_create_subscriptions_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('subscriptions', function (Blueprint $table) {
+            $table->id('subscription_id');
+            $table->string('produk');
+            $table->decimal('harga', 10, 2);
+            $table->date('due_date');
+            $table->unsignedBigInteger('frequency');
+            $table->string('payment_method');
+            $table->unsignedBigInteger('user_id');
+            $table->text('note')->nullable();
+            $table->timestamps();
+    
+            $table->foreignId('frequency')->references('frequency_id')->on('frequencies');
+            $table->foreignId('user_id')->references('user_id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('subscriptions');
+    }
+};
---
+++ database/migrations/2024_12_12_072749_create_payments_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('payments', function (Blueprint $table) {
+            $table->id('payment_id'); 
+            $table->date('payment_date'); 
+            $table->decimal('amount', 10, 2); 
+            $table->unsignedBigInteger('subscription_id'); 
+            $table->string('payment_method'); 
+            $table->string('proof')->nullable(); 
+            $table->timestamps();
+    
+            $table->foreignId('subscription_id')->references('subscription_id')->on('subscriptions');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('payments');
+    }
+};
