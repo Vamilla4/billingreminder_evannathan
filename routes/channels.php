8e5aeb3da249824002bc822c1472b468ad4cfa34
Vamilla4 <evanaradhy@gmail.com>
2024-12-12 14:13:29 +0700

    Update channels.php

--- routes/channels.php
+++ routes/channels.php
@@ -4,9 +4,9 @@ use Illuminate\Support\Facades\Broadcast;
 
 /*
 |--------------------------------------------------------------------------
-| Broadcast Channels
+| Broadcast Channels 
 |--------------------------------------------------------------------------
-|
+|test message
 | Here you may register all of the event broadcasting channels that your
 | application supports. The given channel authorization callbacks are
 | used to check if an authenticated user can listen to the channel.
