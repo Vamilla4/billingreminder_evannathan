0677ad9961e9af133a101d6480bf50b3ee6b4cdd
Vamilla4 <evanaradhy@gmail.com>
2024-12-12 15:09:57 +0700

    Models relationship
    May or may not work well but atleast no error showed up

--- app/Models/Subscription.php
+++ app/Models/Subscription.php
@@ -8,4 +8,18 @@ use Illuminate\Database\Eloquent\Model;
 class Subscription extends Model
 {
     use HasFactory;
+    public function user()
+{
+    return $this->belongsTo(User::class, 'user_id', 'user_id');
+}
+
+public function payments()
+{
+    return $this->hasMany(Payment::class, 'subscription_id', 'subscription_id');
+}
+
+public function frequency()
+{
+    return $this->belongsTo(Frequency::class, 'frequency_id', 'frequency_id');
+}
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -6,6 +6,12 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class User extends Model
-{
+{ 
     use HasFactory;
+
+    public function subscriptions()
+{
+    return $this->hasMany(Subscription::class, 'user_id', 'user_id');
+}
+
 }
